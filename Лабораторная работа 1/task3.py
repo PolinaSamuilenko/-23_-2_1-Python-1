f46f3e8685321ef315a66575442e8d7a5c5340e8
PolinaSamuilenko <158211553+PolinaSamuilenko@users.noreply.github.com>
2024-01-31 19:26:28 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+length = len(list_players)
+middle_length = length//2
+first_team = list_players [:middle_length]
+print(first_team)
+second_team = list_players [middle_length:]
+print(second_team)
+# TODO Разделите участников на две команды
